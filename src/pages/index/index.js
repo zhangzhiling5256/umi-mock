2120d5cadb75ddb865f775151d27e5a966d5c91f
张志岭 <40190525+zhangzhiling5256@users.noreply.github.com>
2018-09-18 23:24:30 +0800

    add less (#1)
    agreed to merge

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,4 +1,4 @@
-import styles from './index.css';
+import styles from './index.less';
 
 export default function() {
   return (
@@ -8,6 +8,9 @@ export default function() {
         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
         <li><a href="https://umijs.org/guide/getting-started.html">Getting Started</a></li>
       </ul>
+      <div>
+        aaaaaaaaaaaaaaaaa
+      </div>
     </div>
   );
 }
